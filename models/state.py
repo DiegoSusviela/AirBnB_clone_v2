92cc7fe86d0ecd8459992a8c2eaa6378edac33ee
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-12 18:52:56 -0300

    changed state.py

--- models/state.py
+++ models/state.py
@@ -3,21 +3,32 @@
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)    
-    if (models.storage_used == "db"):
+    """Returns asdasd"""
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state")
     else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Returns asdasd""""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
         @property
         def cities(self):
-            new_dict = []
-            for key, value in storage.all:
-                if (self.id == value.id):
-                  new_dict[key] = value
-            return (new_dict)
+            """Returns asdasd""""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
