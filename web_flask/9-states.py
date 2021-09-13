38a22a7b08cf0a249672388cc65b327e1457d7af
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-12 22:00:07 -0300

    porque me dejo

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/states', strict_slashes=False)
 @app.route('/states/<state_id>', strict_slashes=False)
 def states(state_id=None):
-    """display the states and cities listed in alphabetical order"""
+    """returns"""
     states = storage.all("State")
     if state_id is not None:
         state_id = 'State.' + state_id
@@ -19,7 +19,7 @@ def states(state_id=None):
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """closes the storage on teardown"""
+    """returns"""
     storage.close()
 
 if __name__ == '__main__':
