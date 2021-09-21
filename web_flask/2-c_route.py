6f4d5f9cdd24e432948511a516ebac9f04a80ef7
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-21 13:27:51 -0300

    https://github.com/DiegoSusviela/AirBnB_clone_v2.git

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,20 +4,24 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """returns"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returns"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def cfun(text):
     """returns"""
     return 'C %s' % text.replace("_", " ")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
